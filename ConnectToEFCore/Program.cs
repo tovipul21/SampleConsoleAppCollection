409352a2499a9f1a522f5d2405f1efe4b3f3b0b2
Vipul Mehta <vipul.mehta@accenture.com>
2021-10-20 13:01:23 +0530

    Added new project to solution

---
+++ ConnectToEFCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConnectToEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
