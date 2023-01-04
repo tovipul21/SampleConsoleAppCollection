2f12e5b31b49b010cc806337609a58de7e27d502
Vipul Mehta <67891332+tovipul21@users.noreply.github.com>
2023-01-04 10:51:39 +0530

    Update Program.cs
    Optimized the code to display data from Json file. Also implemented logic to add new data into Json file post getting input from user.

--- DisplayJsonData/Program.cs
+++ DisplayJsonData/Program.cs
@@ -1,34 +1,53 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using System.Linq;
 using System.Text.Json;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        static string jsonFilePath = @"EmployeeDetails.json";
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Do you want to read or write the data? Choose 1 to read and 2 to write.");
+            string readWriteChoice = Console.ReadLine();
+            //var readWriteChoice = Console.ReadLine() ?? "1";
 
+            switch (readWriteChoice)
+            {
+                // Read Json data
+                case "1":
+                    ReadJsonData();
+                    break;
+                // Write Json data
+                case "2":
+                    WriteIntoJsonFile();
+                    break;
+                default:
+                    ReadJsonData();
+                    break;
+            }
+        }
+
+        private static void ReadJsonData()
+        {
             // 1 - Read Json data from Json file and get the data in collction object
-            //var jsonData = File.OpenText(@"EmployeeDetails.json");
-            var jsonVariable = "[{\"empid\" : 1, \"salary\" : 100},{\"empid\" : 5, \"salary\" : 200},{\"empid\" : 4, \"salary\" : 300},{\"empid\" : 3, \"salary\" : 400},{\"empid\" : 2, \"salary\" : 500}]";
-            //var jsonVariable = @"[{""empid"" : 1, ""salary"" : 200}, {""empid"" : 2, ""salary"" : 250},{""empid"" : 3, ""salary"" : 300},{""empid"" : 1, ""salary"" : 200}]";
+            var jsonData = File.ReadAllText(jsonFilePath);
 
             List<Employee> employees = JsonSerializer.Deserialize<List<Employee>>
             (
-                jsonVariable, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+                jsonData, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
             );
 
             // 2 - Sort the employees in descending order of their salary
-            employees.Sort(delegate (Employee x, Employee y) {
-                return y.Salary.CompareTo(x.Salary);
-            });
+            employees = employees.OrderByDescending(emp => emp.Salary).ToList();
 
             // Display sorting output
-            foreach (var item in employees)
-                Console.WriteLine("Emp Id - " + item.EmpId + " and Salary - " + item.Salary);
+            foreach (var emp in employees)
+                Console.WriteLine("Emp Id - " + emp.EmpId + "; Name - " + emp.FirstName + " " + emp.LastName + "; Salary - " + emp.Salary);
 
             // 3 - Read input data
             Console.WriteLine("Please enter index #");
@@ -38,6 +57,79 @@ namespace ConsoleApp1
             var employee = employees[Convert.ToInt32(empId) - 1];
 
             Console.WriteLine(employee.EmpId + " - " + employee.Salary);
+
+            // 5 - Average salary
+            double averageSalary = employees.Average(emp => emp.Salary);
+            Console.WriteLine($"Average salary of all employees is {averageSalary}");
+
+            // 6 - Aggregate salary
+            //var aggregateSalary = employees.Aggregate<Employee, int>(0,(TotalSalary, emp) => TotalSalary += emp.Salary);
+            //Console.WriteLine($"Total Salary of all employees is {aggregateSalary}");
+
+            var allEmployeeName = employees.Aggregate<Employee, string>
+                (
+                    "Employee Name are as follows ",
+                        (empName, emp) => empName += emp.FirstName + " " + emp.LastName + ", ");
+
+            Console.WriteLine(allEmployeeName);
+        }
+
+        /// <summary>
+        /// This method will insert ata into an Json file.
+        /// </summary>
+        private static void WriteIntoJsonFile()
+        {
+            // 1 - Read Json data from Json file and get the data in collection object
+            var jsonData = File.ReadAllText(jsonFilePath);
+
+            List<Employee> employees = JsonSerializer.Deserialize<List<Employee>>
+            (
+                jsonData, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            );
+
+            // 2 - Start reading user input to be put into the employee object
+            var _empId = employees.Max(emp => emp.EmpId);
+
+            // Get input for employee Salary
+            int _salary = AskSalary("Please enter employee salary in integer format");
+
+            // Get Employee First Name data
+            Console.WriteLine("Please enter employee First Name");
+            var _firstName = Console.ReadLine();
+
+            // Get Employee Last Name data
+            Console.WriteLine("Please enter employee Last Name");
+            var _lastName = Console.ReadLine();
+
+            employees.Add(new Employee()
+            {
+                EmpId = Convert.ToInt32(_empId) + 1,
+                Salary = _salary,
+                FirstName = _firstName,
+                LastName = _lastName
+            });
+
+            string json = JsonSerializer.Serialize(employees);
+            File.WriteAllText(jsonFilePath, json);
+        }
+
+        private static int AskSalary(string salaryQuestion)
+        {
+            Console.WriteLine(salaryQuestion);
+
+            int _salary;
+
+            var isSalaryNumber = Int32.TryParse(Console.ReadLine(), out _salary);
+
+            while (!isSalaryNumber)
+            {
+                Console.WriteLine(salaryQuestion);
+                //_salary = Console.ReadLine();
+
+                isSalaryNumber = Int32.TryParse(Console.ReadLine(), out _);
+            }
+
+            return _salary;
         }
     }
 
@@ -45,5 +137,8 @@ namespace ConsoleApp1
     {
         public int EmpId { get; set; }
         public int Salary { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
